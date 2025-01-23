e945781d4e71e65f46a6ed2d108f9422fba4289a
com <com@DESKTOP-JT6VQSS>
2025-01-23 13:46:15 +0900

    dsdsd

--- LoginSystem/src/main/resources/static/common.js
+++ LoginSystem/src/main/resources/static/common.js
@@ -1,10 +1,10 @@
-async function login(mid, mpw){//로그인 함수 분리
+async function login(mid,mpw){//로그인 함수 분리
 	const logindata = {mid:$("#mid").val(),mpw:$("#mpw").val()}
 	const ctype = "application/json"
 	const res = await fetch("/member/login",{method:"post",headers:{"Content-type":ctype},body:JSON.stringify(logindata)})
 	const data = await res.json();
 	return data;
 }
-function logout(logoutui){//로그아웃 함수 분리
+function logout(logoutui){//로그아웃 함수분리
 	logoutui.attr("href","/member/logout").text("로그아웃")
 }
